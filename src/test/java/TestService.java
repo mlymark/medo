502315ad9204bdac49f138bf94f401ecae6b5e8c
mly <dk081mly@163.com>
2017-03-29 18:04:24 +0800

    将jpa 改为EntityManager实现，修改json无线循环问题，添加restful api权限控制

--- src/test/java/TestService.java
+++ src/test/java/TestService.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
